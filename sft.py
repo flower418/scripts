c2286d2087ddd467fd3e7b042865cc9479d974bd
sunflwoer <ziwenxu@stu.pku.edu.cn>
2026-06-08 14:06:06 +0800

    Update bsz

--- sft.py
+++ sft.py
@@ -20,8 +20,8 @@ dataset = load_dataset(
 training_args = SFTConfig(
     output_dir="./sft_22k",
 
-    per_device_train_batch_size=2,
-    gradient_accumulation_steps=4,
+    per_device_train_batch_size=4,
+    gradient_accumulation_steps=2,
     num_train_epochs=3,
 
     learning_rate=2e-5,
